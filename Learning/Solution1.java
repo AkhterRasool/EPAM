2f4d374151c8ee8ce4b8ab6201f08117af29fdf3
Akhter Rasool <akhterrasool48@gmail.com>
2017-12-01 19:36:09 +0530

    Solution to Problem in Learning Phase 1

---
+++ Learning/Solution1.java
@@ -0,0 +1,26 @@
+//Solution to Problem Statement 1 (Learning Session 1)
+//Solution created by Akhter Rasool <akhterrasool48@gmail.com>
+
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        // Complete this line
+        int integers_read = 0, a;
+        
+        // Complete this line
+        while (integers_read < 3) {
+            a = scan.nextInt();
+            System.out.println(a);
+            integers_read++;
+        }
+
+        // Complete this line
+        // Complete this line
+    }
+}
+
+
